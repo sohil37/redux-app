9ba66e5c604626fb08122bf10cd787a49e170ff4
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-04-11 17:09:25 +0530

    initial commit

---
+++ src/components/Shop.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">
+        -
+      </button>
+      <span>Add to cart</span>
+      <button type="button" className="btn btn-primary">
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Shop;
